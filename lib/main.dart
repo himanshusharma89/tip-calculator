92cf372a2e339f5abfdcdd973f5010367aea0d15
Himanshu Sharma <ranubhardwaj89@gmail.com>
2021-05-18 01:12:52 +0530

    dispose method added

--- lib/main.dart
+++ lib/main.dart
@@ -26,6 +26,15 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   @override
+  void dispose() {
+    if (_billController != null && _tipPercentageController != null) {
+      _billController.dispose();
+      _tipPercentageController.dispose();
+    }
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
