38a99ab17d9620e8d44d4d2d3ff790dd10463516
Himanshu Sharma <ranubhardwaj89@gmail.com>
2021-05-19 07:34:06 +0530

    Sound Null Safe

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'amount.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +13,18 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Tip Calculator',
-      theme: ThemeData(),
+      // Use Theming here.
+      theme: ThemeData(
+          // Define AppBar theme for the whole app
+          appBarTheme: AppBarTheme(
+            elevation: 0,
+            centerTitle: true,
+            backgroundColor: Colors.transparent,
+            textTheme:
+                GoogleFonts.nunitoSansTextTheme(Theme.of(context).textTheme),
+          ),
+          textTheme:
+              GoogleFonts.nunitoSansTextTheme(Theme.of(context).textTheme)),
       home: const MyHomePage(),
     );
   }
@@ -25,21 +38,17 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  @override
-  void dispose() {
-    if (_billController != null && _tipPercentageController != null) {
-      _billController.dispose();
-      _tipPercentageController.dispose();
-    }
-    super.dispose();
-  }
+  // Variable to access the calculated bill and total
+  double _totalBill = 0.0, _totalTip = 0.0;
+  // Make sure to restart after adding controllers.
+  final TextEditingController _billController = TextEditingController();
+  final TextEditingController _tipPercentageController =
+      TextEditingController(text: '15'); // Define initial text here
 
   @override
   void dispose() {
-    if (_billController != null && _tipPercentageController != null) {
-      _billController.dispose();
-      _tipPercentageController.dispose();
-    }
+    _billController.dispose();
+    _tipPercentageController.dispose();
     super.dispose();
   }
 
@@ -63,7 +72,12 @@ class _MyHomePageState extends State<MyHomePage> {
               const SizedBox(
                 height: 20,
               ),
-              totalAmount()
+              // Pass the total bill and tip to stateless widget Amount
+              Amount(
+                totalBill: _totalBill
+                    .toStringAsFixed(2), // Fix the total to decimal points
+                totalTip: _totalTip.toStringAsFixed(2),
+              ),
             ],
           ),
         ),
@@ -75,12 +89,27 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget billTotal() {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
-      children: const [
-        Text(
+      children: [
+        const Text(
           'Bill Total',
           style: TextStyle(fontSize: 16),
         ),
-        TextField()
+        TextField(
+          controller: _billController,
+          // Define the type of keyboard to use for input
+          keyboardType: const TextInputType.numberWithOptions(decimal: true),
+          showCursor: true,
+          autofocus: true,
+          onChanged: (val) {
+            _calculateTotal();
+          },
+          // Text style of the input
+          style: const TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+          decoration: const InputDecoration(
+              border: InputBorder.none, // Removes border
+              hintText: 'Enter Bill Amount', // Add hint text
+              hintStyle: TextStyle(fontWeight: FontWeight.normal)),
+        )
       ],
     );
   }
@@ -88,59 +117,41 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget tip() {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
-      children: const [
-        Text(
+      children: [
+        const Text(
           'Tip Percentage',
           style: TextStyle(fontSize: 16),
         ),
-        TextField()
+        TextField(
+          controller: _tipPercentageController,
+          keyboardType: const TextInputType.numberWithOptions(decimal: true),
+          showCursor: true,
+          autofocus: true,
+          onChanged: (val) {
+            _calculateTotal();
+          },
+          style: const TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+          decoration: const InputDecoration(
+              border: InputBorder.none,
+              hintText: 'Enter Tip Percentage',
+              hintStyle: TextStyle(fontWeight: FontWeight.normal)),
+        )
       ],
     );
   }
 
-  Widget totalAmount() {
-    return Padding(
-      padding: const EdgeInsets.all(20),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: const [
-              Text(
-                'Total Tip',
-                style: TextStyle(fontSize: 16),
-              ),
-              Text(
-                '0.0',
-                style: TextStyle(
-                  fontSize: 35,
-                ),
-              ),
-            ],
-          ),
-          const SizedBox(
-            width: 20,
-          ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: const [
-              Text(
-                'Total',
-                style: TextStyle(fontSize: 16),
-              ),
-              Text(
-                '0.0',
-                style: TextStyle(
-                  fontSize: 35,
-                ),
-              ),
-            ],
-          )
-        ],
-      ),
-    );
+  // Function to calculate the total tip and bill
+  void _calculateTotal() {
+    // Check whether the TextField is empty
+    // if not parse the input field text using TextEditingController
+    final _bill =
+        _billController.text.isEmpty ? 0.0 : double.parse(_billController.text);
+    final _tipPercentage = _tipPercentageController.text.isEmpty
+        ? 0.0
+        : double.parse(_tipPercentageController.text);
+    _totalTip = (_bill * _tipPercentage) / 100;
+    _totalBill = _bill + _totalTip;
+    // Update the state with new value and re-run the build method
+    setState(() {});
   }
 }
